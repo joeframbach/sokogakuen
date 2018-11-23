c8d06552a899e9a17b303f10a41dd1c68c8ae22c
Joe Frambach <joe@framba.ch>
2018-11-23 12:27:48 -0800

    Int 3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var cards = [
+const int2 = [
   ['英語', 'えいご', 'English'],
   ['掲示板', 'けいじばん', 'Bulletin board'],
   ['故障', 'こしょう', 'Out of order'],
@@ -55,26 +55,162 @@ var cards = [
   ['コピー機', 'コピーき', 'Copy machine'],
   ['手紙', 'てがみ', 'Letter'],
   ['全部', 'ぜんぶ', 'All, total']
-].map(cardTemplate);
+];
+
+const int3 = [
+  ["用紙", "ようし", "Form"],
+  ["記入", "きにゅう", "To fill out"],
+  ["名前", "なまえ", "Name"],
+  ["呼ぶ", "よぶ", "To call"],
+  ["待つ", "まつ", "To wait"],
+  ["入る", "はいる", "To enter"],
+  ["心配", "しんぱい", "To worry"],
+  ["便利", "べんり", "Convenient"],
+  ["言う", "いう", "To say"],
+  ["飲む", "のむ", "To drink"],
+  ["休む", "やすむ", "To rest"],
+  ["寝る", "ねる", "To sleep"],
+  ["安い", "やすい", "Cheap"],
+  ["痛い", "いたい", "Painful"],
+  ["相談", "そうだん", "Consultation"],
+  ["独身", "どくしん", "Single"],
+  ["受付", "うけつけ", "Reception"],
+  ["病院", "いいん", "Hospital"],
+  ["初診", "しょしん", "First visit"],
+  ["内科", "ないか", "Internal medicine"],
+  ["外科", "げか", "Surgery"],
+  ["眼科", "がんか", "Eye-ology"],
+  ["歯科", "しか", "Dentistry"],
+  ["耳鼻科", "じびか", "Ear/nose/throat-ology"],
+  ["薬", "くすり", "Medicine"],
+  ["薬局", "やっきょく", "Pharmacy"],
+  ["病気", "びょうき", "Sick"],
+  ["医者", "いしゃ", "Doctor"],
+  ["患者", "かんじゃ", "Patient"],
+  ["総合", "そうごう", "General (Hospital)"],
+  ["医院", "びょういん", "Clinic"],
+  ["入院", "にゅういん", "Hospitalization"],
+  ["三階", "さんがい", "Third floor"],
+  ["愛", "こい", "Love"],
+  ["お金", "おかね", "Money"],
+  ["大切", "たいせつ", "Important"],
+  ["両方", "りょうほう", "Both"],
+  ["寝る", "ねる", "To sleep"],
+  ["最近", "さいきん", "Recently"],
+  ["流行", "りゅうこう", "Fashion"],
+  ["店員", "てんいん", "Clerk"],
+  ["聞く", "きく", "To listen"],
+  ["試着室", "しちゃくしつ", "Dressing room"],
+  ["試す", "ためす", "To try"],
+  ["明るい", "あかるい", "Bright"],
+  ["色", "いろ", "Color"],
+  ["無地", "むじ", "Plain"],
+  ["日記", "にっき", "Diary"],
+  ["右", "みぎ", "Right"],
+  ["左", "ひだり", "Left"],
+  ["辞書", "じしょ", "Dictionary"],
+  ["世界", "せかい", "World"],
+  ["物価", "ぶっか", "Cost of living"],
+  ["値段", "ねだん", "Price of product"],
+  ["髪", "かみ", "Hair"],
+  ["会う", "あう", "To meet"],
+  ["雨", "あめ", "Rain"],
+  ["雪", "ゆき", "Snow"],
+  ["降る", "ふる", "To fall"],
+  ["着物", "きもの", "Kimono"],
+  ["都市", "とし", "City"],
+  ["人口", "じんこう", "Population"],
+  ["赤い", "あかい", "Red"],
+  ["背が高い", "せがたかい", "Tall"],
+  ["書店", "しょてん", "Bookstore"],
+  ["本屋", "ほんや", "Bookstore"],
+  ["同じ", "おなじ", "Same"],
+  ["紹介", "しょうかい", "Introduction"],
+  ["友人", "ゆうじん", "Friend (more formal)"],
+  ["注文", "ちゅうもん", "Order"],
+  ["住所", "じゅうしょ", "Street address"],
+  ["氏名", "しめい", "Full name"],
+  ["出版社", "しゅっばんしゃ", "Publishing company"],
+  ["題名", "だいめい", "Book title"],
+  ["著者", "ちょしゃ", "Author"],
+  ["研究", "けんきゅう", "Labratory"],
+  ["連絡", "れんらく", "Communication"],
+  ["情報", "じょうほう", "Information"],
+  ["昨日", "きのう", "Yesterday"],
+  ["一緒", "いっしょ", "Together"],
+  ["学校", "がっこう", "School"],
+  ["地理", "ちり", "Geography"],
+  ["教育", "きょういく", "Education"],
+  ["生物", "せいぶつ", "Biology"],
+  ["語学", "ごがく", "Linguistics"],
+  ["会計", "かいけい", "Accounting"],
+  ["科学", "かがく", "Science"],
+  ["哲学", "てつがく", "Philosophy"],
+  ["法律", "ほうりつ", "Law"],
+  ["経済", "けいざい", "Economics"],
+  ["経営", "けいえい", "Management"],
+  ["新刊書", "しんかんしょ", "New book"],
+  ["欲しい", "ほしい", "Wanted"],
+  ["授業", "じゅぎょう", "Lesson"],
+  ["読む", "よむ", "To read"],
+  ["決める", "きめる", "To decide"],
+  ["交番", "こうばん", "Police box"],
+  ["交差点", "こうさてん", "Intersection"],
+  ["手前", "てまえ", "This side"],
+  ["向い側", "むかいがわ", "Other side"],
+  ["市役所", "しやくしょ", "City hall"],
+  ["通行人", "つうこうにん", "Passerby"],
+  ["男", "おとこ", "Man"],
+  ["女", "おんな", "Woman"],
+  ["遠い", "とおい", "Far away"],
+  ["近い", "ちかい", "Close/nearby"],
+  ["目印", "めじるし", "Landmark"],
+  ["バス停", "ばすてい", "Bus stop"],
+  ["電車", "でんしゃ", "Train"],
+  ["乗る", "のる", "To get on"],
+  ["降りる", "おりる", "To get off"],
+  ["歩く", "あるく", "To walk"],
+  ["神社", "じんじゃ", "Shrine"],
+  ["お寺", "おてら", "Temple"],
+  ["道", "みち", "Road"],
+  ["歩道橋", "ほどうきょう", "Footbridge"],
+  ["渡る", "渡る", "To cross over"],
+  ["映画館", "えいがかん", "Movie Theater"],
+  ["電気", "でんき", "Electric"],
+  ["製品", "せいひん", "Product"],
+  ["消防署", "しょうぼうしょ", "Fire station"],
+  ["行く", "いく", "To go"],
+  ["曲がる", "まがる", "To turn"],
+  ["先週末", "せんしゅうまつ", "Last weekend"],
+  ["困る", "こまる", "To be at a loss"],
+  ["有名", "ゆうめい", "Famous"],
+  ["買う", "かう", "To buy"],
+  ["漢字", "かんじ", "Kanji"],
+];
+
+const levels = {
+  "level-int-2": int2,
+  "level-int-3": int3
+}
 
 function deal() {
-  document.querySelector('#cards').innerHTML = shuffle(cards).slice(0,10).join('');
+  const cards = levels[document.querySelector("[name=level]:checked").id];
+  document.querySelector('#cards').innerHTML = shuffle(cards).slice(0,10).map(cardTemplate).join('');
 }
 deal();
 
 function cardTemplate ([kanji, hiragana, english]) {
+  const content = `
+    <p class="kanji">${kanji}</p>
+    <p class="hiragana">${hiragana}</p>
+    <p class="english">${english}</p>
+  `;
   return `
-    <input id="done-${kanji}" type="checkbox" />
     <input id="show-${kanji}" type="checkbox" />
-    <div class="card">
-      <label for="show-${kanji}">
-        <label for="done-${kanji}">
-          <p class="kanji answer">${kanji}</p>
-          <p class="hiragana answer">${hiragana}</p>
-          <p class="english answer">${english}</p>
-        </label>
-      </label>
-    </div>`;
+    <label class="card hide" for="show-${kanji}">${content}</label>
+    <input id="done-${kanji}" type="checkbox" />
+    <label class="card" for="done-${kanji}">${content}</label>
+    </label>`;
 }
 
 // https://stackoverflow.com/a/2450976/1253312
